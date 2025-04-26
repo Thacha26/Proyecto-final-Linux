76a7806c0a884434d9826a339fb14065c5a2812d
Thacha26 <fridathais26@ciencias.unam.mx>
2025-04-26 15:15:44 -0600

    Se sube la primera parte del main

---
+++ Main.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+echo -e "\t██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗"
+echo -e "\t██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝"
+echo -e "\t██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  "
+echo -e "\t██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  "
+echo -e "\t╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗"
+echo -e "\t ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝"
+echo -e "\t    								 "
+
+#Se bloquean lso comandos de Ctrl z y c
+trap '' SIGINT  # Bloquea Ctrl+C
+trap '' SIGTSTP # Bloquea Ctrl+Z
+
+echo -e "\t ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "\t Holaaa, tenga la más cordial bienvenida a esta grandiosa terminal"
+echo -e "\t      Por favor, ingrese su usuario y contraseña para acceder"
+echo -e "\t ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+
+salir=false
+while [[ $salir == false ]]; do
+	echo -n "Usuario: "
+        read -r username
+        echo -n "Contraseña: "
+        read -r -s password
+        echo ""
+
+#Ahora se debe de verificar que el usuario y contraseña existan
+if su -c "exit" "$username" <<< "$password" 2>/dev/null; then
+        echo "Muy bien, puede acceder ahora"
+        exit 0
+else
+echo -e "\t ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+	echo -e "\tLo lamento, el usuario o contraseña son incorrectos"
+        echo -e "\tPor favor, elija alguna de las siguientes opciones"
+        echo -e "\t1. Volver a intentar"
+        echo -e "\t2. Salir"
+echo -e "\t ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+read -r opcion
+case "$opcion" in
+	1)
+		continue #Si selecciona 1 continúa
+		;;
+	2) #Si selecciona 2 sale del prgrama
+		salir=true
+		echo "Nos vemosssss"
+		echo "----Usted está saliendo de la terminal----"
+                ;;
+        *) #Si selecciona alguna opción fuera de las dadas marca un error
+                echo "Opción fuera del rango, intenta de nuevo"
+                ;;
+	esac #Se termina el swich
+	fi #se cierra el if
+done #se cierra el done
+
