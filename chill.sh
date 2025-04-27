f4a00133ac072946b0d950a329b4228e6d4de394
Thacha26 <fridathais26@ciencias.unam.mx>
2025-04-26 23:03:33 -0600

    Se hizo más interactivo el meme

--- chill.sh
+++ chill.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-echo -e "\tCrea a tu chico chill de cojones, o chilln't de cojones"
-echo -e "\t\tpor ejemplo"
-echo -e "\t\tPov: aún no terminas el proyecto"
-echo -e "\t\tPero estás chill de cojones\n"
-echo -e ""
-read -p "Por favor ingrese su texto " respuesta
+validar_opcion() {
+    while true; do
+        read -p "Será chill o chilln't (1 o 2)? " opcion
+        case "$opcion" in
+            1)
+                chill="chill"
+                break
+                ;;
+            2)
+                chill="chilln't"
+                break
+                ;;
+            *)
+                echo "Opción fuera del rango, intenta de nuevo"
+                ;;
+        esac
+    done
+}
+
+validar_opcion
+
+read -p "Por favor ingrese su texto: " respuesta
+
+clear
 echo -e "\t POV: $respuesta"
+echo -e "\t Pero tu eres un chicx $chill de cojones"
 echo -e "\t\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣶⠀⢀⣴⣶⡄⠀⠀⠀"
 echo -e "\t\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣞⣿⢠⡟⣿⣿⠇⠀⠀⠀"
 echo -e "\t\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣨⣿⣿⣼⣿⣟⠏⠀⠀⠀⠀"
